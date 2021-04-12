6c321d0b169aa76ade618bebe1d6ded2e82a6523
AndreaTrento95 <trentora95@gmail.com>
2021-04-12 16:11:00 +0200

    svolgimento

--- js/script.js
+++ js/script.js
@@ -18,9 +18,21 @@ cognomi.push(cognome_utente);
 cognomi.sort();   
 
 // Stampo la lista ordinata alfabeticamente
+
+
 for(var i = 0; i < cognomi.length; i++){
     cognome = cognomi[i];
     
     var elemento_precedente = document.getElementById("lista").innerHTML;
     document.getElementById("lista").innerHTML = elemento_precedente + "<li>"+ cognome +"</li>";
+
+    var trovato = false;
+    while(trovato === false){
+        if(cognome === nome_utente){
+            trovato = true;
+            console.log("TROVATO");
+        }
+        i++;
+    }
+    
 }
